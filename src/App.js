ae12f17f67248c2ac57610386d053b3029148014
hasharas <hasharamsankalpam@gmail.com>
2024-03-08 23:58:44 +0530

    Adding card,footer,analytics,hero,newsletter jsx file and add some icons for this.finish this web page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Navbar from './component/Navbar';
+import Analytics from './components/Analytics';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
-    <div className="App" >
-     <Navbar />
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
